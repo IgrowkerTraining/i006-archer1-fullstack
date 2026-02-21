254d7e109bd19a7c9b83e9a8fe6c5dc5243a9183
Marta <159387209+martaic@users.noreply.github.com>
2026-02-21 14:44:59 +0100

    error solucionado

--- apps/frontend/src/routes/AppRoutes.tsx
+++ apps/frontend/src/routes/AppRoutes.tsx
@@ -1,42 +1,51 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
+import { ExplotationProvider } from "../context/ExplotationContext";
 
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Dashboard from "../pages/Dashboard";
+import InitialPage from "../pages/InitialPage";
+import HomeProductor from "../pages/HomeProductor";
+import ActivityRegister from "../pages/ActivityRegister";
+import TechnicalAddExplotation from "../pages/TechnicalAddExplotation";
+import HomeTecnic from "../pages/HomeTecnic";
+import ProducerHistory from "../pages/ProducerHistory";
+import TechnicalHistory from "../pages/TechnicalHistory";
 
 export const AppRoutes: React.FC = () => {
   return (
-    <Routes>
-      <Route
-        path="/login"
-        element={
-          <PublicRoute>
-            <Login />
-          </PublicRoute>
-        }
-      />
+    <ExplotationProvider>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
 
-      <Route
-        path="/register"
-        element={
-          <PublicRoute>
-            <Register />
-          </PublicRoute>
-        }
-      />
-      <Route
-        path="/actividad"
-        element={
-          <ProtectedRoute>
-            <ActividadRegistro />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/" element={<Navigate to="/login" replace />} />
-    </Routes>
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+
+        <Route path="/" element={<InitialPage />} />
+        <Route path="/homeProductor" element={<HomeProductor />} />
+        <Route path="/homeTecnic" element={<HomeTecnic />} />
+        <Route path="/activity" element={<ActivityRegister />} />
+        <Route path="/producer-history" element={<ProducerHistory />} />
+        <Route path="/technical-history" element={<TechnicalHistory />} />
+        <Route path="/add-explotation" element={<TechnicalAddExplotation />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </ExplotationProvider>
   );
 };
 
