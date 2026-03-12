ee54d9fc1cc32924a8ebb210c7b5972573b6cd40
JimCosta <jimmyhorseman95@gmail.com>
2026-03-12 13:41:54 -0300

    "correction in the routes"

--- apps/backend/src/routes/activity.ts
+++ apps/backend/src/routes/activity.ts
@@ -54,7 +54,7 @@ router.post("/", activityController.createActivity);
  *               items:
  *                 $ref: '#/components/schemas/Activity'
  */
-router.get("",authenticateJWT, activityController.getActivities);
+router.get("/",authenticateJWT, activityController.getActivities);
 
 /**
  * @openapi
@@ -82,7 +82,7 @@ router.get("",authenticateJWT, activityController.getActivities);
  *             schema:
  *               $ref: '#/components/schemas/ActivityFormOptionsResponse'
  */
-router.get("",authenticateJWT, activityController.getFormOptions);
+router.get("/",authenticateJWT, activityController.getFormOptions);
 
 /**
  * @openapi
